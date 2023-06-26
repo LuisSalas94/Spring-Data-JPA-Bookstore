efa0d5ce4b09542a6f04a7cf4f36999b290170d0
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-26 04:32:10 -0500

    Create Book Entity

---
+++ src/main/java/com/fernandosalasbookstore/entity/Book.java
@@ -0,0 +1,37 @@
+package com.fernandosalasbookstore.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String title;
+
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String description;
+
+    private BigDecimal price;
+
+    private String genre;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @CreationTimestamp
+    private LocalDateTime creationTime;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+}
