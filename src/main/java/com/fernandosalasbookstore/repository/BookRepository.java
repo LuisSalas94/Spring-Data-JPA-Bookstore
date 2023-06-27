c8a5dd7f23eb1d28ffb55e017dc785e9527da13d
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-27 06:45:53 -0500

    Add findByTitle Method

--- src/main/java/com/fernandosalasbookstore/repository/BookRepository.java
+++ src/main/java/com/fernandosalasbookstore/repository/BookRepository.java
@@ -3,4 +3,5 @@ import com.fernandosalasbookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByTitle(String title);
 }
---
+++ src/test/java/com/fernandosalasbookstore/repository/QueryMethodTest.java
@@ -0,0 +1,19 @@
+package com.fernandosalasbookstore.repository;
+import com.fernandosalasbookstore.entity.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QueryMethodTest {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void findByTitleMethod() {
+        Book book = bookRepository.findByTitle("Cosmos");
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthor());
+    }
+
+}
