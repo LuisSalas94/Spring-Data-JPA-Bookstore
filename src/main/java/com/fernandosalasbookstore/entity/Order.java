d01efa155851065d12471c8078af149efa83ab10
Fernando Salas <luisfernandosalasg@gmail.com>
2023-07-03 04:11:41 -0500

    Create Order Entity

---
+++ src/main/java/com/fernandosalasbookstore/entity/Order.java
@@ -0,0 +1,30 @@
+package com.fernandosalasbookstore.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String orderTrackingNumber;
+
+    private int totalQuantity;
+
+    private BigDecimal totalPrice;
+
+    private String status;
+
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+}
