6fb585a655397537a73dac114c680d6f76841037
Fernando Salas <luisfernandosalasg@gmail.com>
2023-07-11 02:25:47 -0500

    Establish Unidirectional Many-To-Many mapping

--- src/main/java/com/fernandosalasbookstore/entity/User.java
+++ src/main/java/com/fernandosalasbookstore/entity/User.java
@@ -1,6 +1,7 @@
 package com.fernandosalasbookstore.entity;
 import jakarta.persistence.*;
 import lombok.Data;
+import java.util.Set;
 
 @Entity
 @Data
@@ -23,4 +24,16 @@ public class User {
     private String email;
 
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"
+            )
+    )
+    private Set<Role> roles;
 }
